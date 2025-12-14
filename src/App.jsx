f813bf43bdd7ec0ba053ced29bb232cc3cef3999
GeoCarter96 <geolcarter21@gmail.com>
2025-12-14 02:29:21 -0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
 import Home from './pages/Home/Home';
 import {Routes, Route, useNavigate} from 'react-router-dom';
-import Login from "./pages/Home/Login/Login";
-import Player from './pages/Home/Player/Player';
+import Login from "./components/Login/Login";
+import Player from './components/Player/Player';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from './firebase';
 import { ToastContainer, toast } from 'react-toastify';
