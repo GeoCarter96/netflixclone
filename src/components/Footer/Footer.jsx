a341dd0dafba119e55756b0ae3bacff66b644d5d
GeoCarter96 <geolcarter21@gmail.com>
2025-12-13 22:38:00 -0500

    add

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,6 +14,21 @@ const Footer = () => {
         <img src={twitter_icon} alt="" />
         <img src={facebook_icon} alt="" />
       </div>
+      <ul>
+        <li>Audio Description</li>
+        <li>Help Center</li>
+        <li>Gift Cards</li>
+        <li>Media Center</li>
+        <li>Investor Relations</li>
+        <li>Jobs</li>
+        <li>Terms Of Use</li>
+        <li>Privacy</li>
+        <li>Legal Notices</li> 
+        <li>Cookie Preferences</li>
+        <li>Corporate Info</li>
+        <li>Contact Us</li>
+      </ul>
+      <p className='copyright-text'>&copy; 1997-2025 Netflix, Inc</p>
     </div>
   )
 }
