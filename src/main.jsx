b2802f9994f814bc1831bdbc18f57ac2ebd5d204
GeoCarter96 <geolcarter21@gmail.com>
2025-12-14 11:58:22 -0500

    add

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,11 @@ import './index.css'
 import App from './App.jsx'
 import  {BrowserRouter as Router} from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <Router>
       <App />
     </Router>
    
-  </React.StrictMode>,
-)
+  </StrictMode>,
+);
